b5ce16bc03e42f561bfedf36714554d1c8630c87
Gabriel Brito Santos <108184514+Gaelbsgg@users.noreply.github.com>
2024-06-24 12:24:16 -0300

    Add check-box

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // * Ligando os elementos Js com as classes do HTML
-
 let sliderElement = document.querySelector("#slider");
 let buttonElement = document.querySelector("#button");
 
@@ -8,29 +7,48 @@ let password = document.querySelector("#password");
 
 let containerPassword = document.querySelector("#container-password");
 
-// * Definindo qual vai ser os caracteres que podem ser usados para a senha ser gerada,
+let maiusculoElement = document.querySelector("#maiusculo");
+let minusculoElement = document.querySelector("#minusculo");
+let numerosElement = document.querySelector("#numeros");
 
-let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
 let novaSenha = "";
 
 // * Definindo qual a quantidade de caracteres contidos na senha a partir do "slider"
 
 sizePassword.innerHTML = sliderElement.value;
 
-slider.oninput = function(){
+sliderElement.oninput = function(){
     sizePassword.innerHTML = this.value;
 }
 
-// * Função que gera a senha com base no valor definido pelo client e dentro do charset (randômica)
+// * Função que gera a senha com base no valor definido pelo cliente e dentro do charset (randômica)
 
 function generatePassword(){
-    let pass= "";
+    let charset = "";
+
+    if (maiusculoElement.checked) {
+        charset += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    }
+    if (minusculoElement.checked) {
+        charset += "abcdefghijklmnopqrstuvwxyz";
+    }
+    if (numerosElement.checked) {
+        charset += "1234567890";
+    }
+
+    if (charset === "") {
+        alert("Por favor, selecione ao menos um tipo de caractere.");
+        return;
+    }
+
+    let pass = "";
     
     for(let i = 0, n = charset.length; i < sliderElement.value; ++i){
         pass += charset.charAt(Math.floor(Math.random() * n));
-        }
+    }
  
-// * Elemento que vai remover e "revelar a senha gerada"        
+    // * Elemento que vai remover e "revelar a senha gerada"        
+
     containerPassword.classList.remove("hide");
     password.innerHTML = pass;
     novaSenha = pass;
